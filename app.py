a509249c3eb7365c520858026ca1c9fd26b40dc0
Trần Nguyễn Hoàng Huy <promegasniper2412@gmail.com>
2024-11-21 19:36:08 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Hello<h1>"
+
+@app.route('/<name>')
+def user(name):
+    return f"Hello {name}"
+
+if __name__ == "__main__":
+    app.run()
