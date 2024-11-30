6da49488a3c580f9131343d1581952fe5953d671
Lê Kim Quang <quang2312tp@gmail.com>
2024-11-30 09:37:58 +0700

    quang

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello<h1>"
+    return render_template('home.html')
 
-@app.route('/<name>')
-def user(name):
-    return f"Hello {name}"
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
