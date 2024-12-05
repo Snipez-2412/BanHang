de4500d34e3a1bc3c4d00fbf1a5fda34799921fe
Chau <tnchau23@gmail.com>
2024-12-05 07:47:12 +0700

    Update Database

--- app.py
+++ app.py
@@ -2,13 +2,6 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "<h1>Hello<h1>"
-
-@app.route('/<name>')
-def user(name):
-    return f"Hello {name}"
 
 if __name__ == "__main__":
     app.run()
--- database.py
+++ database.py
@@ -7,14 +7,15 @@ db = client['my_database']
 phone = db['phone']
 
 db.phone.insert_one({
-    "ten":"Redmi 13C",
-    "hang":"Redmi",
-    "title":"(Starfrost White, 6GB RAM, 128GB Storage) | Powered by 4G MediaTek Helio G85 | 90Hz Display | 50MP AI Triple Camera",
-    "gia":"8.000.000 đồng",
-    "url_image":"https://m.media-amazon.com/images/I/71scmEdSC2L._AC_UY218_.jpg",
+    "ten":"Oneplus Nord CE4",
+    "hang":"OnePlus",
+    "title":"Oneplus Nord CE4 (Celadon Marble, 8GB RAM, 128Gb Storage)",
+    "gia":"10.500.000 đồng",
+    "url_image":"https://m.media-amazon.com/images/I/61nxQ62qglL._AC_UY218_.jpg",
 })
+print("Thanh công")
 
-docs = phone.find({"hang":"Redmi"})
-for doc in docs:
-    print(doc)
+# docs = phone.find({"hang":"Realme"})
+# for doc in docs:
+#     print(doc)
 # redmi realme iphone oneplus
